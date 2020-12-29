a283aec11cc3a07a7a1f09b821be7d6041c3d47b
79635 <2007eia2408@gmail.com>
2020-12-29 11:23:02 +0700

    kniga

--- app/src/main/java/com/arbonik/kniga/MainActivity.kt
+++ app/src/main/java/com/arbonik/kniga/MainActivity.kt
@@ -3,29 +3,74 @@ package com.arbonik.kniga
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.StringRes
 
+data class Question(@StringRes val textResId:Int,
+                    val answer: Boolean)
 class MainActivity : AppCompatActivity() {
     lateinit var True:Button
     lateinit var False:Button
+    lateinit var Next:ImageButton
+    lateinit var Prev:ImageButton
+    lateinit var Ques:TextView
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia,true))
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         True = findViewById(R.id.True)
         False = findViewById(R.id.False)
+        Next = findViewById(R.id.Next)
+        Prev = findViewById(R.id.imageButton2)
+        Ques = findViewById(R.id.question)
+        Next.setOnClickListener{
+            currentIndex = (currentIndex+1)%questionBank.size
+            updateQuestion()
+        }
+        Prev.setOnClickListener {
+            if (currentIndex==0){
+                currentIndex=1
+            }
+            currentIndex = (currentIndex-1)%questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
         True.setOnClickListener {
-            Toast.makeText(
-                this,
-                R.string.corc_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            currentIndex = (currentIndex+1)%questionBank.size
+            updateQuestion()
+            checkAnswer(true)
         }
         False.setOnClickListener {
-            Toast.makeText(
-                this,
-                R.string.incr_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            currentIndex = (currentIndex+1)%questionBank.size
+            updateQuestion()
+            checkAnswer(false)
         }
+        Ques.setOnClickListener {
+            currentIndex = (currentIndex+1)%questionBank.size
+            updateQuestion()
+        }
+    }
+    fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        Ques.setText(questionTextResId)
+    }
+    fun checkAnswer(userAnswer:Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer){
+            R.string.corc_toast
+        }else{
+            R.string.incr_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_LONG)
+            .show()
     }
 }
